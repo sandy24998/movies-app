62389443a044eb081b934e6bfa8676cf090927cc
sandy24998 <24panditsandeep@gmail.com>
2019-06-26 01:37:45 +0530

    login style added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -24,7 +24,7 @@ const customStyles = {
 
 const TabContainer = function (props) {
     return (
-        <Typography component="div" style={{ padding: 0 }}>
+        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
             {props.children}
         </Typography>
     )
@@ -69,7 +69,7 @@ class Header extends Component{
                     onRequestClose={this.closeModalHandler}
                     style={customStyles}>
 
-                        <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login" />
                         <Tab label="Register" />
                     </Tabs>
@@ -78,11 +78,13 @@ class Header extends Component{
                             <InputLabel htmlFor="username">Username</InputLabel>
                             <Input id="username" type="text" />
                         </FormControl>
-
+                            <br/><br/>
                         <FormControl required>
                             <InputLabel htmlFor="password">Password</InputLabel>
                             <Input id="password" type="password" />
                         </FormControl>
+                        <br /><br />
+                        <Button variant="contained" color="primary">LOGIN</Button>
                     </TabContainer>
                 </Modal>
             </div>
