9856b14de633f1531296d33bad2497f3ae668b36
sandy24998 <24panditsandeep@gmail.com>
2019-07-17 19:41:48 +0530

    filter1 added

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,13 +1,116 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
+import { withStyles } from '@material-ui/core/styles';
+import moviesData from '../../common/movieData';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
-class Home extends Component{
-    render(){
-        return(
-            <div><Header /></div>
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    upcomingMoviesHeading: {
+        textAlign: 'center',
+        background: '#ff9999',
+        padding: '8px',
+        fontSize: '1rem'
+    },
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+     },
+     title: {
+        color: theme.palette.primary.light,
+     }
+});
+
+class Home extends Component {
+
+    constructor(){
+        super();
+        this.state ={
+            movieName:""
+        }
+    }
+
+    movieNameChangeHandler = event =>{
+        this.setState({movieName: event.target.value});
+        console.log(this.state.movieName);
+    }
+
+
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <Header />
+                <div className={classes.upcomingMoviesHeading}>
+                    <span>Upcoming Movies</span>
+                </div>
+                <GridList cols={5} className={classes.gridListUpcomingMovies}>
+                    {moviesData.map(movie =>(
+                        <GridListTile key={movie.id}>
+                            <img src={movie.poster_url} className="movie-poster" alt={movie.title}/>
+                            <GridListTileBar title={movie.title}/>
+                        </GridListTile>     
+                    ))}
+                </GridList>
+
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                            <Card>
+                                <CardContent>
+                                    <FormControl className={classes.formControl}>
+                                        <Typography className={classes.title}>
+                                            FIND MOVIES BY:
+                                        </Typography>
+                                    </FormControl>
+                                    <FormControl className={classes.formControl}>
+                                        <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                                        <Input id="movieName" onChange={this.movieNameChangeHandler}/>
+                                    </FormControl>
+                                </CardContent>
+                            </Card>
+                    </div>
+                </div>       
+
+            </div>
         )
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
